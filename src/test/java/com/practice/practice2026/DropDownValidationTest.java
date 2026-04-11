958b7f279d2dfa1fc0b8e824a54132f290bb449f
unknown <nayaksantappa87@gmail.com>
2026-04-12 01:51:05 +0530

    Removed blank line

--- src/test/java/com/practice/practice2026/DropDownValidationTest.java
+++ src/test/java/com/practice/practice2026/DropDownValidationTest.java
@@ -11,7 +11,6 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
-
 import com.practice.Interview.pages.DemoAutoTestPage;
 
 public class DropDownValidationTest {
